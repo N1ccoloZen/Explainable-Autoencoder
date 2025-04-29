f23eb71c7c105cdc4eaef3ca0060cdfa5484679e
Niccolò Zenaro <niccolozen5@gmail.com>
2025-04-29 12:47:14 +0200

    conceptloader

--- matfileloader.py
+++ matfileloader.py
@@ -2,25 +2,51 @@ import scipy.io
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 name = sys.argv[1]
 
 mat = scipy.io.loadmat(name)
-
-print(mat.keys())
-
+print("Key features:", mat.keys())
+#print(mat.keys())
 anno = mat['anno']
 #df = pd.DataFrame(matrix)
 #np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-
 #df = pd.DataFrame(mat)
-
 #print(anno.shape)
 
 anno_struct = anno[0, 0]
 
 print(anno_struct.dtype.names)
 
-print(anno_struct['imname'])
+print("Image name:", anno_struct['imname'])
 
-print(anno_struct['objects'])
+for i in range(anno_struct['objects'].shape[1]):
+    #print(anno_struct['objects'][0,i][3][0])
+    #print("Object", i, ":", anno_struct['objects'][0,i])
+    print("Object", i ,"name:", anno_struct['objects'][0,i][0][0])
+    parts = anno_struct['objects'][0,i][3]
+    print(parts.shape)
+    for i in range(parts.shape[1]):
+        part = parts[0, i]
+        label = part[0][0]
+
+        mask = part[1]
+        if mask.sum() > 0:
+            print("This mask exists")
+            print("Concept ", label, "exists in photo:", anno_struct['imname'])
+
+        #if label == 'head':
+            #print("This is the mask:")
+            #mask = part[1]
+            #if mask.sum() > 0:
+            #   print("This mask exists")
+            #print(f"Shape: {mask.shape}")
+            #plt.imshow(mask, cmap='gray')
+            #plt.show()
+
+print("detected obj:", anno_struct['objects'].size)
+
+
+
+
