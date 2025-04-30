5e84af65758ad4505cff0be4bbc610b35b6f31f7
Niccolò Zenaro <niccolozen5@gmail.com>
2025-04-30 17:31:12 +0200

    dir access

--- matfileloader.py
+++ matfileloader.py
@@ -3,23 +3,39 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-name = sys.argv[1]
+dirName = sys.argv[1]
 
-mat = scipy.io.loadmat(name)
-print("Key features:", mat.keys())
-#print(mat.keys())
-anno = mat['anno']
-#df = pd.DataFrame(matrix)
-#np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-#df = pd.DataFrame(mat)
-#print(anno.shape)
+concepts_list = []
+
+for i, files in enumerate(os.listdir(dirName)):
+    print("File name:", files)
+
+    if  files == '2008_000652.mat':
+        filename = os.path.join(dirName, files)
+        mat = scipy.io.loadmat(filename)
+        print("Key features:", mat.keys())
+        anno = mat['anno']
+        anno_struct = anno[0, 0]
+
+        print(anno_struct.dtype.names)
+
+        print("Image name:", anno_struct['imname'])
+        break
+
+print(i)
+
+""" mat = scipy.io.loadmat(dirName)
+print("Key features:", mat.keys()) """
+
+""" anno = mat['anno']
 
 anno_struct = anno[0, 0]
 
 print(anno_struct.dtype.names)
 
-print("Image name:", anno_struct['imname'])
+print("Image name:", anno_struct['imname'])  """
 
 for i in range(anno_struct['objects'].shape[1]):
     #print(anno_struct['objects'][0,i][3][0])
@@ -33,8 +49,8 @@ for i in range(anno_struct['objects'].shape[1]):
 
         mask = part[1]
         if mask.sum() > 0:
-            print("This mask exists")
-            print("Concept ", label, "exists in photo:", anno_struct['imname'])
+            #print("This mask exists")
+            print("Concept", label, "exists in photo:", anno_struct['imname'])
 
         #if label == 'head':
             #print("This is the mask:")
@@ -46,7 +62,3 @@ for i in range(anno_struct['objects'].shape[1]):
             #plt.show()
 
 print("detected obj:", anno_struct['objects'].size)
-
-
-
-
